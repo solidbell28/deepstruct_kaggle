1795ebedf3f7b5b5ab8a2cf98a7883384f0f323e
Ilya Tsybanov <ilyatcybanov@gmail.com>
2024-10-24 23:00:07 +0300

    path fix

--- src/dataset_processing/datasets.py
+++ src/dataset_processing/datasets.py
@@ -488,7 +488,7 @@ class JointERDataset(BaseDataset):
         """
         examples = []
         name = self.name if self.data_name is None else self.data_name
-        file_path = os.path.join(self.data_dir(), f'{name}_{split}.json')
+        file_path = os.path.join(self.data_dir(), f'{name}_{split}.json')[2:] # removing first ../
 
         with open(file_path, 'r') as f:
             data = json.load(f)
