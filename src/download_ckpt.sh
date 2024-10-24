8817f122b33c477a9fed04d84e32550cb3a5f1e7
Ilya Tsybanov <ilyatcybanov@gmail.com>
2024-10-24 21:41:12 +0300

    model download fix

--- src/download_ckpt.sh
+++ src/download_ckpt.sh
@@ -1,4 +1,5 @@
-cp -rf src/* ./
-pip install -r requirements.txt --user
-mkdir ../data
-mkdir ../ckpt
+mkdir ../ckpt/MP
+mkdir ../ckpt/MP/2B
+
+wget https://huggingface.co/Magolor/deepstruct/resolve/main/hub/MP/2B/mp_rank_00_model_states.pt
+mv mp_rank_00_model_states.pt ../ckpt/MP/2B
