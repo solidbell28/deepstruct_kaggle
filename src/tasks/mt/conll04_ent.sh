64c7138a788e1f0ae3fc6c2137e7ef28c14b9b8f
Ilya Tsybanov <s02220279@gse.cs.msu.ru>
2024-10-31 14:14:45 +0300

    test

--- src/tasks/mt/conll04_ent.sh
+++ src/tasks/mt/conll04_ent.sh
@@ -3,4 +3,4 @@ python3 manager.py --model-type model_blocklm_10B \
                    --task conll04 \
                    --task-epochs 0 \
                    --length-penalty 0.8 \
-                   --num-gpus-per-node 8
+                   --num-gpus-per-node 2
