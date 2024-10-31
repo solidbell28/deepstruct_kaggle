316565cf154c50172a66228f4d85f14612f2ac26
Ilya Tsybanov <s02220279@gse.cs.msu.ru>
2024-10-31 14:04:23 +0300

    10B

--- src/download_ckpt.sh
+++ src/download_ckpt.sh
@@ -1,5 +1,5 @@
 mkdir ../ckpt/MP
-mkdir ../ckpt/MP/2B
+mkdir ../ckpt/MP/10B
 
-wget https://huggingface.co/Magolor/deepstruct/resolve/main/hub/MP/2B/mp_rank_00_model_states.pt
-mv mp_rank_00_model_states.pt ../ckpt/MP/2B
+wget https://huggingface.co/Magolor/deepstruct/resolve/main/hub/MP/10B/mp_rank_00_model_states.pt
+mv mp_rank_00_model_states.pt ../ckpt/MP/10B
--- src/tasks/mt/conll04_ent.sh
+++ src/tasks/mt/conll04_ent.sh
@@ -1,6 +1,6 @@
-python3 manager.py --model-type model_blocklm_2B \
-                   --model-checkpoint ../../ckpt/MP/2B/ \
+python3 manager.py --model-type model_blocklm_10B \
+                   --model-checkpoint ../../ckpt/MP/10B/ \
                    --task conll04 \
                    --task-epochs 0 \
                    --length-penalty 0.8 \
-                   --num-gpus-per-node 2
+                   --num-gpus-per-node 8
