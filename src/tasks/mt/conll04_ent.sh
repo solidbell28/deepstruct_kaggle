3364fd8eb67e8432d81cdb5af1f8c026e31385ef
Ilya Tsybanov <s02220279@gse.cs.msu.ru>
2024-10-31 13:53:07 +0300

    quick fix

--- src/tasks/mt/conll04_ent.sh
+++ src/tasks/mt/conll04_ent.sh
@@ -2,5 +2,5 @@ python3 manager.py --model-type model_blocklm_2B \
                    --model-checkpoint ../../ckpt/MP/2B/ \
                    --task conll04 \
                    --task-epochs 0 \
-                   --length-penalty 0.8
+                   --length-penalty 0.8 \
                    --num-gpus-per-node 2
