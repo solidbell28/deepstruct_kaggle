bec348b1d226a47216d2b78507e3f2b19c1dfa2e
Ilya Tsybanov <s02220279@gse.cs.msu.ru>
2024-10-31 16:11:01 +0300

    cultural code dataset add

---
+++ src/dataset_processing/process_cult_code.py
@@ -0,0 +1,30 @@
+import json
+
+
+def preprocess(path):
+    with open(path, 'r') as file:
+      data = json.load(file)['dataset']
+    span_tags = data['span_tags']
+    res = []
+    for markup in data['markups']:
+        tokens = markup['text'].split()
+        entities = []
+        for span in markup['spans']:
+            entity = {
+                "type": span_tags[span['id']],
+                "start": span['begin'],
+                "end": span['end']
+            }
+            entities.append(entity)
+        elem = {
+            "tokens": tokens,
+            "entitites": entities
+        }
+        res.append(elem)
+    output = json.dumps(res)
+    with open(path, 'w') as file:
+        file.write(output)
+
+
+if __name__ == "__main__":
+    preprocess('../data/cult_code/cult_code.json')
--- src/tasks/mt/cult_code_ent.sh
+++ src/tasks/mt/cult_code_ent.sh
@@ -1,6 +1,6 @@
 python3 manager.py --model-type model_blocklm_2B \
                    --model-checkpoint ../../ckpt/MP/2B/ \
-                   --task conll04 \
+                   --task ccode \
                    --task-epochs 0 \
                    --length-penalty 0.8 \
                    --num-gpus-per-node 2
