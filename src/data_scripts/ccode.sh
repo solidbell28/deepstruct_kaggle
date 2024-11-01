1ee5ae66e89c9eb22c25eed84bf153a2c6095ba3
Ilya Tsybanov <s02220279@gse.cs.msu.ru>
2024-11-01 15:56:32 +0300

    name inconsistency fix

---
+++ src/data_scripts/ccode.sh
@@ -0,0 +1,2 @@
+cp -rf /kaggle/input/cultural-code ../data/ccode
+python3 dataset_processing/process_ccode.py
--- src/data_scripts/cult_code.sh
+++
@@ -1,2 +0,0 @@
-cp -rf /kaggle/input/cultural-code ../data/cult_code
-python3 dataset_processing/process_cult_code.py
